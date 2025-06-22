5f9d92264be55138a87f55a7c5614269bb2dd911
Rajkumar-pr <139167030+Rajkumar-pr@users.noreply.github.com>
2025-06-22 09:38:03 +0530

    Update Memo.jsx

--- src/Memo.jsx
+++ src/Memo.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React,{ueState,useMemo} from "react";
 
 function Memo(){
@@ -22,29 +22,4 @@ Double of data is
 
         </div>
     )
-=======
-import React,{ueState,useMemo} from "react";
-
-function Memo(){
-    const [count,setCount]=useState(0);
-    const [data,setData]=useState(1);
-    const [double,setDouble]=useState(1);
-    const handleChange=(e)=>{
-        setData(e.target.value);
-        setDouble(2*data);
-
-    }
-    return(
-        useMemo()
-        <div>
-increase <button onClick={setCount((prev)=>(prev+1))}>
-Add 1
-</button>
-count is:{count}
-Double of data is 
-<input type="text" value={data} onChange={handleChange}/>
-
-        </div>
-    )
->>>>>>> b2923ed75085a17916702d9fd8ef0d4092383c4b
-}
+            }
