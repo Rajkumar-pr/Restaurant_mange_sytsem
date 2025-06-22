e87b2879048457765e23590f87a7bd4860349a66
Rajkumar-pr <139167030+Rajkumar-pr@users.noreply.github.com>
2025-06-22 08:59:47 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 import { configureStore } from "@reduxjs/toolkit";
 import countReducer from "./state/counter";
 export default configureStore({
     reducer:{
          count:countReducer
     }
-=======
-import { configureStore } from "@reduxjs/toolkit";
-import countReducer from "./state/counter";
-export default configureStore({
-    reducer:{
-         count:countReducer
-    }
->>>>>>> b2923ed75085a17916702d9fd8ef0d4092383c4b
+
 })
