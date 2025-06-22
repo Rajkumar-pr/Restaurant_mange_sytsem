30514ed0881e32060f0fc3e7d70847b76e61605f
Rajkumar-pr <139167030+Rajkumar-pr@users.noreply.github.com>
2025-06-22 09:29:22 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useState } from 'react';
 import { Card, CardContent, Typography, Button, CardActions } from '@mui/material';
 
@@ -44,50 +44,4 @@ function ProductCard({ name, description, image, price,onAddtoCart,onRemoveCard 
 }
 
 export default ProductCard;
-=======
-import React, { useState } from 'react';
-import { Card, CardContent, Typography, Button, CardActions } from '@mui/material';
-
-function ProductCard({ name, description, image, price,onAddtoCart,onRemoveCard }) {
-  // State to manage the visibility of the full description
-  const [isExpanded, setIsExpanded] = useState(false);
-
- 
-  const handleReadMore = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  return (
-    <Card sx={{ width: 300, margin: 2 }}>
-      <img src={image} alt={name} style={{ height: 200, width: '100%', objectFit: 'cover' }} />
-      <CardContent>
-        <Typography gutterBottom variant="h6">
-          {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {isExpanded ? description : `${description.substring(0, 100)}...`}
-        </Typography>
-
-        {/* Read More button */}
-        <Button onClick={handleReadMore} color="primary">
-          {isExpanded ? 'Read Less' : 'Read More'}
-        </Button>
-
-        <Typography variant="h6" color="green" mt={1}>
-          ${price}
-        </Typography>
-      </CardContent>
-   <CardActions>
-    <Button onClick={onAddtoCart}>
-        Add To Cart
-    </Button>
-    <Button onClick={onRemoveCard}>
-      Remove From Cart
-    </Button>
-   </CardActions>
-    </Card>
-  );
-}
-
-export default ProductCard;
->>>>>>> b2923ed75085a17916702d9fd8ef0d4092383c4b
+   
